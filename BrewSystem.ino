4039e6e8eb4c606d5548e964f4e17f3958b571f4
GevenM <gevenm@gmail.com>
2015-07-04 11:33:21 -0400

    Added logfile to webserver

--- BrewSystem.ino
+++ BrewSystem.ino
@@ -185,6 +185,12 @@ IPAddress ip(192,168,0, 112);
 byte gateway[] = { 192, 168, 0, 1 };
 byte subnet[] = { 255, 255, 255, 0 };
 
+// size of buffer used to capture HTTP requests
+#define REQ_BUF_SZ   20
+File webFile;                    // handle to files on SD card
+char HTTP_req[REQ_BUF_SZ] = {0}; // buffered HTTP request stored as null terminated string
+char req_index = 0;              // index into HTTP_req buffer
+
 unsigned int localPort = 8888;      // local port to listen for UDP packets
 IPAddress timeServer(206, 108, 0,131); //ntp server
 const int timeZone = -4;
@@ -571,7 +577,20 @@ void loop()
 		String dataString = "";
 		
 		// print time and date
-		dataString += String(hour()) + String(minute()) + String(second()) + " " + String(day()) + "/" + String(month()) + "/" + String(year());
+		if ( hour() < 10 ){
+			dataString += "0";
+		}
+		dataString += String(hour()) + ":";
+		
+		if ( minute()<10){
+			dataString += "0";
+		}
+		dataString += String(minute()) + ":" ;
+		
+		if ( second()<10){
+			dataString += "0";
+		}
+		dataString += String(second()) + " " + String(day()) + "/" + String(month()) + "/" + String(year());
 		
 		// print item
 		dataString += "," + String(m_temp_hlt->GetName()) ;
@@ -580,7 +599,7 @@ void loop()
 		dataString += "," + String(m_temp_hlt->GetTemp());
 	
 		// open file to log to
-		File logFile = SD.open("logFile.txt", FILE_WRITE );
+		File logFile = SD.open("logFile.csv", FILE_WRITE );
 		
 		// check if available and write to it. 
 		if( logFile ){
@@ -880,44 +899,46 @@ void loop()
 		while (client.connected()) {
 			if (client.available()) {
 				char c = client.read();
-			//	Serial.write(c);
-				// if you've gotten to the end of the line (received a newline
-				// character) and the line is blank, the http request has ended,
-				// so you can send a reply
+				// buffer first part of HTTP request in HTTP_req array (string)
+				// leave last element in array as 0 to null terminate string (REQ_BUF_SZ - 1)
+				if (req_index < (REQ_BUF_SZ - 1)) {
+					HTTP_req[req_index] = c;          // save HTTP request character
+					req_index++;
+				}
+				Serial.print(c);    // print HTTP request character to serial monitor
+				// last line of client request is blank and ends with \n
+				// respond to client only after last line received
 				if (c == '\n' && currentLineIsBlank) {
 					// send a standard http response header
 					client.println("HTTP/1.1 200 OK");
 					client.println("Content-Type: text/html");
-					client.println("Connection: close");  // the connection will be closed after completion of the response
-					client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+					client.println("Connnection: close");
 					client.println();
-					client.println("<!DOCTYPE HTML>");
-					client.println("<html>");
-					client.println("NEW LIMBURG BREWERY <br>");
-				
-					client.println("Brew System");
-					client.println("<br><br>");
-					client.println("Temperatures:<br>");
-					for ( int sensor = 0 ; sensor < TempSensor::GetNumberOfSensors() ; sensor++ ){
-						client.println( tempSensor[sensor].GetName() );
-						client.println(": ");
-						if ( tempSensor[sensor].IsPresent()){
-							client.println( tempSensor[sensor].GetTemp() );
-							client.println( " Celcius. <br>");
-						} else {
-							client.println( "not detected.<br>");
-						}
-					
+					// open requested web page file
+					if (StrContains(HTTP_req, "GET / ")
+					|| StrContains(HTTP_req, "GET /index.htm")) {
+						webFile = SD.open("index.htm");        // open web page file
 					}
-
-					client.println("</html>");
+					else if (StrContains(HTTP_req, "GET /logFile.csv")) {
+						webFile = SD.open("logFile.csv");        // open web page file
+					}
+					// send web page to client
+					if (webFile) {
+						while(webFile.available()) {
+							client.write(webFile.read());
+						}
+						webFile.close();
+					}
+					// reset buffer index and all buffer elements to 0
+					req_index = 0;
+					StrClear(HTTP_req, REQ_BUF_SZ);
 					break;
 				}
+				// every line of text received from the client ends with \r\n
 				if (c == '\n') {
 					// you're starting a new line
 					currentLineIsBlank = true;
-				}
-				else if (c != '\r') {
+				} else if (c != '\r') {
 					// you've gotten a character on the current line
 					currentLineIsBlank = false;
 				}
@@ -929,8 +950,6 @@ void loop()
 		client.stop();
 		//Serial.println("client disconnected");
 	}
-	
-
 }
 
 void InitDisplays(){
@@ -1466,4 +1485,43 @@ void sendNTPpacket(IPAddress &address)
 	Udp.beginPacket(address, 123); //NTP requests are to port 123
 	Udp.write(packetBuffer, NTP_PACKET_SIZE);
 	Udp.endPacket();
+}
+
+
+// sets every element of str to 0 (clears array)
+void StrClear(char *str, char length)
+{
+	for (int i = 0; i < length; i++) {
+		str[i] = 0;
+	}
+}
+
+// searches for the string sfind in the string str
+// returns 1 if string found
+// returns 0 if string not found
+char StrContains(char *str, char *sfind)
+{
+	char found = 0;
+	char index = 0;
+	char len;
+
+	len = strlen(str);
+	
+	if (strlen(sfind) > len) {
+		return 0;
+	}
+	while (index < len) {
+		if (str[index] == sfind[found]) {
+			found++;
+			if (strlen(sfind) == found) {
+				return 1;
+			}
+		}
+		else {
+			found = 0;
+		}
+		index++;
+	}
+
+	return 0;
 }
